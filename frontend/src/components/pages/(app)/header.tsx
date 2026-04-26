0796d4408197790cf15ca22ebe5a2db1c9cd8208
maulana-tech <firdaussyah03@gmail.com>
2026-04-26 23:51:21 +0700

    refactor(frontend/src/components/pages/(app)/header.tsx): auto update

--- frontend/src/components/pages/(app)/header.tsx
+++ frontend/src/components/pages/(app)/header.tsx
@@ -21,7 +21,7 @@ export function Header() {
               <path d="M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z" />
             </svg>
           </div>
-          <span className="text-base font-semibold text-text-main">Custogen</span>
+          <span className="text-base font-semibold text-text-main">Cusygen</span>
         </Link>
         <nav className="flex items-center gap-1">
           {NAV_ITEMS.map((item) => {
